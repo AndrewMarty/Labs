2b1ebacec01f677ea44f656cf951a39bdbd448c0
Андрей <martynkevich.2000@gmail.com>
2023-06-15 20:11:18 +0300

    add

---
+++ Maxim 10/Maxim 10/Maxim 10.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+// Функция для печати стека
+void printStack(stack<int> st) {
+    while (!st.empty()) {
+        cout << '\t' << st.top();
+        st.pop();
+    }
+    cout << '\n';
+}
+
+// Функция для поиска минимального и максимального элемента и их обмена
+stack<int> swapMinMax(stack<int> st) {
+    stack<int> temp;
+    int min_val = numeric_limits<int>::max();
+    int max_val = numeric_limits<int>::min();
+    while (!st.empty()) {
+        int val = st.top();
+        min_val = min(min_val, val);
+        max_val = max(max_val, abs(val));
+        st.pop();
+        temp.push(val);
+    }
+
+    // Заменить минимальный элемент на max_val и максимальный элемент на min_val
+    st = temp;
+    while (!temp.empty()) {
+        temp.pop();
+    }
+    while (!st.empty()) {
+        int val = st.top();
+        if (val == min_val) {
+            temp.push(max_val);
+        }
+        else if (abs(val) == max_val) {
+            temp.push(min_val);
+        }
+        else {
+            temp.push(val);
+        }
+        st.pop();
+    }
+    return temp;
+}
+
+// Функция для удаления всех неотрицательных чисел
+stack<int> removeNonNegatives(stack<int> st) {
+    stack<int> temp;
+    while (!st.empty()) {
+        int val = st.top();
+        if (val < 0) {
+            temp.push(val);
+        }
+        st.pop();
+    }
+    return temp;
+}
+
+void clearStack(stack<int>& st) {
+    while (!st.empty()) {
+        st.pop();
+    }
+}
+
+int main() {
+    stack<int> st;
+
+    // Заполнение стека числами от -50 до 50
+    for (int i = -50; i <= 50; ++i) {
+        st.push(i);
+    }
+
+    cout << "Initial stack: " << endl;
+    printStack(st);
+
+    st = swapMinMax(st);
+
+    cout << "After swapping min and max: " << endl;
+    printStack(st);
+
+    st = removeNonNegatives(st);
+
+    cout << "After removing non-negatives: " << endl;
+    printStack(st);
+    clearStack(st);
+    return 0;
+}
---
+++ Maxim 11/Maxim 11/Maxim 11.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+// Функция для печати очереди
+void printQueue(queue<int> q) {
+    while (!q.empty()) {
+        std::cout << '\t' << q.front();
+        q.pop();
+    }
+    std::cout << '\n';
+}
+
+// Функция для определения, сколько элементов находятся после максимального
+int countAfterMax(queue<int> q) {
+    int max_val = numeric_limits<int>::min();
+    int max_pos = -1;
+    int i = 0;
+
+    queue<int> copy = q;
+    while (!copy.empty()) {
+        if (copy.front() > max_val) {
+            max_val = copy.front();
+            max_pos = i;
+        }
+        copy.pop();
+        ++i;
+    }
+
+    return i - max_pos - 1;
+}
+
+// Функция для удаления четных положительных и нечетных отрицательных чисел
+queue<int> filterQueue(queue<int> q) {
+    queue<int> new_q;
+    while (!q.empty()) {
+        int val = q.front();
+        if (!((val > 0 && val % 2 == 0) || (val < 0 && val % 2 != 0))) {
+            new_q.push(val);
+        }
+        q.pop();
+    }
+    return new_q;
+}
+
+// Функция для сортировки очереди по модулю
+queue<int> sortQueue(queue<int> q) {
+    vector<int> temp;
+    while (!q.empty()) {
+        temp.push_back(q.front());
+        q.pop();
+    }
+    sort(temp.begin(), temp.end(), [](int a, int b) {
+        return abs(a) < abs(b);
+        });
+
+    for (int val : temp) {
+        q.push(val);
+    }
+    return q;
+}
+
+// Функция для очистки очереди
+void clearQueue(queue<int>& q) {
+    while (!q.empty()) {
+        q.pop();
+    }
+}
+
+int main() {
+    queue<int> q;
+
+    // Заполнение очереди числами от -50 до 50
+    for (int i = -50; i <= 50; ++i) {
+        q.push(i);
+    }
+
+    cout << "Initial queue: " << std::endl;
+    printQueue(q);
+
+    int count = countAfterMax(q);
+    cout << "Elements after max: " << count << endl;
+
+    q = filterQueue(q);
+    cout << "After filtering: " << endl;
+    printQueue(q);
+
+    q = sortQueue(q);
+    cout << "After sorting: " << endl;
+    printQueue(q);
+
+    // Очистка очереди перед выходом из программы
+    clearQueue(q);
+
+    return 0;
+}
